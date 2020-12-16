40dcf58e0ca2afe0ef564fada1c619bb3baed9fb
Justin Pak <justinpak13@gmail.com>
2020-12-15 21:39:44 -0500

    Added the Industry Graph

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ import dash_html_components as html
 import plotly.express as px
 import dash_table
 import base64
+import plotly.graph_objs as go
 
-zipcode = 22152
+zipcode = 22315
 
 naics = pd.read_csv(r'C:\Users\justi\Python Projects\PPP\PPP-Analysis\PPP Data\6-digit_2017_Codes.csv',encoding='latin1')
 naics = naics.drop('Unnamed: 2',axis=1)
@@ -39,19 +40,34 @@ dff = dff[dff['Zip'] == zipcode]
 
 dff = dff.merge(naics, on = 'NAICSCode')
 
-# #using the sample dataset
-# dff = pd.read_csv('sample.csv')
+dff_industry = dff.groupby('2017 NAICS Title').sum().sort_values('LoanAmount',ascending=False)
 
-zips = dff['Zip'].dropna().unique()
+fig = px.bar(dff_industry.iloc[:10], x=dff_industry.index[:10], y='LoanAmount').update_xaxes(categoryorder="total descending")
+fig.update_layout(margin=dict(
+        l=10,
+        r=10,
+        b=200,
+        t=100,
+        pad=5),
+        xaxis_title="Industries",
+        title="Top 10 Industries sorted by total loan amount",
+        title_x=0.5)
+fig.update_xaxes(tickangle=25, tickfont=dict(size=14))
+
+
 
 app = dash.Dash()
 app.layout = html.Div(children=[
-    html.H1(children='Data visualization of PPP Loans based on ZIP code'),
-    html.Div(children="Please enter how many loans you would like to see"),
-    dcc.Input(id='input', value='10', type='text'),
+    html.H1(children=f'Data of PPP Loans based on ZIP code: {zipcode}'),
     html.Div(id='total_number'),
     html.Div(id='total_amount'),
+    dcc.Graph(
+        id='industry_graph',
+        figure=fig),
+    html.Div(html.P([ html.Br()])),
     html.H3(id="top"),
+    html.Div(children="Please enter how many loans you would like to see"),
+    dcc.Input(id='input', value='10', type='text'),
     dash_table.DataTable(
         id='datatable',
         columns = [
@@ -65,14 +81,15 @@ app.layout = html.Div(children=[
 @app.callback(
     [Output(component_id='total_number', component_property='children'),
      Output(component_id='total_amount', component_property='children'),
-    Output(component_id='top', component_property='children'),
+     Output(component_id='top', component_property='children'),
      Output(component_id='datatable', component_property='data')],
     [Input(component_id='input', component_property='value')])
 def update_values(input_data):
+
     return (f"Total Number of Businesses within {zipcode}: {str(len(dff[dff['Zip']==int(zipcode)]))}", #Returns the total amount of businesses that recieved help in the zip
             f"Total amount of money lent within {zipcode}: {round(dff[dff['Zip'] == int(zipcode)]['LoanAmount'].sum(),2):,}",
             f"Top {input_data} Businesses that recieved PPP Loans by the amount recieved:",
-            dff[dff['Zip']==int(zipcode)][:min(len(dff),int(input_data))].sort_values('LoanAmount',ascending=False).to_dict('records'))
+            dff.sort_values('LoanAmount',ascending=False)[:min(len(dff),int(input_data))].to_dict('records'))
 
 
 
